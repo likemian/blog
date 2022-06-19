b5a91450ba65c0b25f2fb8cc6d95ef925ad7abdb
likemian <18624269120@163.com>
2022-06-19 11:43:38 +0800

    feat: vue init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+require('./bootstrap');
+window.Vue = require('vue');
+window.VueRouter = require('vue-router');
+import routes from "./router"
+import axios from "axios"
+import ElementPlus from 'element-plus'
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+import RootComponent from "./components/layouts/App"
+const app = Vue.createApp(RootComponent)
+app.config.globalProperties.$http=axios
+app.use(router)
+   .use(ElementPlus);
+app.mount('#app')
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <p>home</p>
+</template>
+<script>
+  export default {
+
+  methods:{
+
+      cs(){
+
+          axios.get("../api/index").then(function (response){
+
+              console.log(response);
+
+          }).catch(function (error){
+
+              console.log(error);
+
+          })
+
+      }
+
+  },
+
+  mounted() {
+
+      this.cs();
+
+  }}
+</script>
+  <style scoped>
+  </style>
